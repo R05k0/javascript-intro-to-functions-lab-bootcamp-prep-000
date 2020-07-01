9da2e0609bbb6f79bf94766ed718cb16cf6783ea
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-01 22:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function shout (string) {
-return string.toUpperCase()
+  return string.toUpperCase()
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string .toLowerCase()
 }
-
-function sayHiToGrandma("Hello")
+function logShout(string) {
+  console.log(toUpperCase)
+}
